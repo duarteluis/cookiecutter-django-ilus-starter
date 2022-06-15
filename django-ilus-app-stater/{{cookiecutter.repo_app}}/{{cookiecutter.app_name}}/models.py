df3d2623afef8c0e50bad3b2996fe90d9420caff
ld <l.duarte@optologic.ch>
2022-06-15 09:22:02 +0200

    solve error on models {%- if .. -%}

--- django-ilus-app-stater/{{cookiecutter.repo_app}}/{{cookiecutter.app_name}}/models.py
+++ django-ilus-app-stater/{{cookiecutter.repo_app}}/{{cookiecutter.app_name}}/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils.translation import gettext_lazy as _
 
-{% if cookiecutter.app_type == "UUID" %}
+{% if cookiecutter.app_type == "UUID" -%}
 import uuid
-{% elif cookiecutter.app_type == "Slug" %}
+{% elif cookiecutter.app_type == "Slug" -%}
 from autoslug import AutoSlugField
 {% endif %}
 
@@ -12,13 +12,13 @@ from autoslug import AutoSlugField
 
 
 class {{ cookiecutter.model_name }}(models.Model):
-    { % if cookiecutter.app_type == "UUID" %}
+    {%- if cookiecutter.app_type == "UUID" -%}
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    { % endif %}
+    {%- endif %}
     name = models.CharField(max_length=255)
-    { % if cookiecutter.app_type == "Slug" %}
+    {%- if cookiecutter.app_type == "Slug" -%}
     slug = AutoSlugField(populate_from='name', blank=True, editable=True,)
-    { % endif %}
+    {%- endif %}
 
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True, editable=False)
@@ -27,15 +27,15 @@ class {{ cookiecutter.model_name }}(models.Model):
         ordering = ('-created', )
 
     def __unicode__(self):
-        { % if cookiecutter.app_type == "UUID" %}
+        {%- if cookiecutter.app_type == "UUID" -%}
         return f'{self.name} ({self.id}'
-        { % elif cookiecutter.app_type == "Slug" %}
+        {%- elif cookiecutter.app_type == "Slug" -%}
         return self.slug
-        { % endif %}
+        {% endif %}
 
     def get_absolute_url(self):
-        { % if cookiecutter.app_type == "UUID" %}
+        {%- if cookiecutter.app_type == "UUID" -%}
         return reverse('{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_read', args=(self.id, ))
-        { % elif cookiecutter.app_type == "Slug" %}
+        {%- elif cookiecutter.app_type == "Slug" -%}
         return reverse('{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_read', args=(self.slug, ))
-        { % endif %}
+        {% endif %}
