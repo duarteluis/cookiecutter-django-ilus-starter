1842adb146cbc6b343292927985f80230724b5d8
ld <l.duarte@optologic.ch>
2022-06-15 11:58:05 +0200

    resolve urls reverse

--- django-ilus-app-stater/{{cookiecutter.repo_app}}/{{cookiecutter.app_name}}/models.py
+++ django-ilus-app-stater/{{cookiecutter.repo_app}}/{{cookiecutter.app_name}}/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 {%- if cookiecutter.app_type == "UUID" -%}
