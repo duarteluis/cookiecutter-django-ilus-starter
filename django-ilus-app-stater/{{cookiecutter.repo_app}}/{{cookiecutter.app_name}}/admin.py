67701b696596a366125d01c9d9ee1e3be1a3ec5a
ld <l.duarte@optologic.ch>
2022-06-16 00:24:04 +0200

    some fix

--- django-ilus-app-stater/{{cookiecutter.repo_app}}/{{cookiecutter.app_name}}/admin.py
+++ django-ilus-app-stater/{{cookiecutter.repo_app}}/{{cookiecutter.app_name}}/admin.py
@@ -1,24 +1,11 @@
+
 from django.contrib import admin
-from django.shortcuts import redirect
 
-from .models import {{ cookiecutter.model_name }}
-from .forms import {{ cookiecutter.model_name }}Form
+from .models import
+
+{{cookiecutter.model_name}}
 
 
-class AdminRedirectMixin(object):
-    def response_add(self, request, obj, post_url_continue=None):
-        if not any(('_continue' in request.POST, '_addanother' in request.POST)):
-            return redirect(obj.get_absolute_url())
-        return super(AdminRedirectMixin, self).response_add(request, obj, post_url_continue)
-
-    def response_change(self, request, obj):
-        if not any(('_continue' in request.POST, '_addanother' in request.POST)):
-            return redirect(obj.get_absolute_url())
-        return super(AdminRedirectMixin, self).response_change(request, obj)
-
-class {{ cookiecutter.model_name }}Admin(AdminRedirectMixin, admin.ModelAdmin):
-    form = {{ cookiecutter.model_name }}Form
+@admin.register({{cookiecutter.model_name}})
+class {{cookiecutter.model_name}}Admin(admin.ModelAdmin):
     list_display = ('name', 'created', 'updated')
-
-
-admin.site.register({{ cookiecutter.model_name }}, {{ cookiecutter.model_name }}Admin)
--- django-ilus-app-stater/{{cookiecutter.repo_app}}/{{cookiecutter.app_name}}/urls.py
+++ django-ilus-app-stater/{{cookiecutter.repo_app}}/{{cookiecutter.app_name}}/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import patterns, url
+from django.urls import path
 
-
-urlpatterns = patterns('{{ cookiecutter.app_name }}.views',
-    url(r'^$', '{{ cookiecutter.model_name|lower }}_list', name='{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_list'),
-    url(r'^create/$', '{{ cookiecutter.model_name|lower }}_create', name='{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_create'),
-    url(r'^(?P<slug>\S+)/update/$', '{{ cookiecutter.model_name|lower }}_update', name='{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_update'),
-    url(r'^(?P<slug>\S+)/$', '{{ cookiecutter.model_name|lower }}_detail', name='{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_read'),
-)
+urlpatterns = [
+    path('', '{{ cookiecutter.model_name|lower }}_list',
+         name='{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_list'),
+    path('create/', '{{ cookiecutter.model_name|lower }}_create',
+         name='{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_create'),
+    path('<slug>/update/', '{{ cookiecutter.model_name|lower }}_update',
+         name='{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_update'),
+    path('<slug>/', '{{ cookiecutter.model_name|lower }}_read',
+         name='{{ cookiecutter.app_name }}_{{ cookiecutter.model_name|lower }}_read'),
+]
